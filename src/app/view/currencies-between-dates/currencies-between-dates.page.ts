cfda4e2a22dee045c438589bffdf2f39b92e3dd4
rbaldini69 <rbaldini6975@gmail.com>
2026-05-25 09:51:00 -0300

    disposicion de grafico y validacion de fecha

--- src/app/view/currencies-between-dates/currencies-between-dates.page.ts
+++ src/app/view/currencies-between-dates/currencies-between-dates.page.ts
@@ -5,6 +5,8 @@ import{IonicModule} from '@ionic/angular';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { HttpClientService } from 'src/app/service/http-client';
 import { Chart } from 'chart.js/auto';
+import { IDivisa } from 'src/types';
+
 
 @Component({
   selector: 'app-currencies-between-dates',
@@ -16,8 +18,16 @@ import { Chart } from 'chart.js/auto';
 export class CurrenciesBetweenDatesPage implements OnInit {
 
   constructor(private httpClientService:HttpClientService) { }
-  divisas: any[] = [];
-  moneda: string = 'EUR';
+  filteredDivisas: IDivisa[] = [];
+  vistaSeleccionada: string = 'lista';
+chart:any;
+searchTerm: string = '';
+showResults:boolean=false;
+divisaSeleccionada: IDivisa|null=null;
+divisas: IDivisa[] = [];
+moneda: string = 'USD';
+fechaActual: string =
+  new Date().toISOString().split('T')[0];
 fechaDesde: string = '';
 fechaHasta: string = '';
 data:any
@@ -42,6 +52,28 @@ data:any
     }
   });
   }
+  validarFechas() {
+
+  if (
+    this.fechaDesde &&
+    this.fechaHasta &&
+    this.fechaDesde > this.fechaHasta
+  ) {
+
+    this.fechaHasta = this.fechaDesde;
+
+  }
+
+  if (
+    this.fechaHasta &&
+    this.fechaHasta > this.fechaActual
+  ) {
+
+    this.fechaHasta = this.fechaActual;
+
+  }
+
+}
 buscarCotizaciones() {
   this.httpClientService.getCurrenciesBetweenDate(
     this.moneda,
@@ -49,8 +81,12 @@ buscarCotizaciones() {
     this.fechaHasta
   ).subscribe({
     next: (res) => {
+      console.log("resultados")
       console.log(res);
       this.data = res;
+      if (this.chart) {
+      this.chart.destroy();
+    }
       this.crearGrafico();
     },
     error: (err) => {
@@ -59,8 +95,12 @@ buscarCotizaciones() {
   });
 }
 crearGrafico() {
+if (this.chart) {
 
-    new Chart('miGrafico', {
+  this.chart.destroy();
+
+}
+    this.chart=new Chart('miGrafico', {
 
       type: 'line',
 
@@ -72,13 +112,23 @@ crearGrafico() {
 
         datasets: [
           {
-            label: 'EUR',
-            data: this.data.results.map(
-            (x:any) => x.detalle[0].tipoCotizacion
+            //label:'dolar',
+            label:this.moneda,
+            data: this.data.results.map((x:any) => x.detalle[0].tipoCotizacion
           ),
             tension: 0.3
           }
         ]
+      },
+      options:{
+        responsive:true,
+        plugins:{
+          title:{
+            display:true,
+            text: this.data.results[0].detalle[0].descripcion
+            
+          }
+        }
       }
 
     });
@@ -89,11 +139,23 @@ crearGrafico() {
   this.httpClientService.getDivisa()
     .subscribe({
 
-      next: (res:any) => {
+      next: (res) => {
 
         console.log(res);
 
-        this.divisas = res.results;
+        this.divisas = res.results.map((item: any): IDivisa => ({
+
+        id: item.id,
+
+        codigoMoneda: item.codigo,
+
+        descripcion: item.denominacion,
+
+        tipoPase: item.tipoPase,
+
+        tipoCotizacion: item.tipoCotizacion
+
+      }));
 
       },
 
@@ -104,6 +166,47 @@ crearGrafico() {
       }
 
     });
+
+}
+filterCurrencies(event: any) {
+
+  const value = event.detail.value?.toLowerCase() || '';
+
+  if (!value.trim()) {
+
+    this.filteredDivisas = [];
+
+    this.showResults = false;
+
+    return;
+  }
+
+  this.filteredDivisas = this.divisas.filter(divisa =>
+
+    divisa.codigoMoneda.toLowerCase().includes(value) ||
+
+    divisa.descripcion.toLowerCase().includes(value)
+
+  );
+
+  this.showResults = true;
+
+}
+seleccionarDivisa(divisa: IDivisa) {
+  console.log(divisa)
+
+  this.divisaSeleccionada = divisa;
+
+  this.moneda=this.divisaSeleccionada.codigoMoneda;
+
+   this.filteredDivisas = [];
+
+  this.showResults = false;
+
+  this.searchTerm = `${divisa.codigoMoneda} - ${divisa.descripcion}`;
+
+
+  //console.log(this.divisaSeleccionada);
 
 }
 }
