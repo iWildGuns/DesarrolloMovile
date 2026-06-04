c0c56d0a34b92fa8ef1fba531a9d4b0be93a6eed
Hector Diaz <hrdr1995@gmail.com>
2026-06-04 19:40:20 -0300

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAj9G9OFb1lNUhxW2GJnHs-oE5yPnxWhjE',
+    authDomain: 'movileproject-e2ee6.firebaseapp.com',
+    projectId: 'movileproject-e2ee6',
+    storageBucket: 'movileproject-e2ee6.firebasestorage.app',
+    messagingSenderId: '420797392681',
+    appId: '1:420797392681:web:4b541772537d80c75ab938',
+    measurementId: 'G-Q91J92K4BP',
+  },
 };
